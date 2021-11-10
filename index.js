6041b9212295067f03cb7f6e6d7b903f0ed45c38
Bokijonov Mukhsinjon <78298971+BokijonovM@users.noreply.github.com>
2021-11-10 14:13:36 +0100

    hh

---
+++ index.js
@@ -0,0 +1,12 @@
+/*<!-- MANDATORY JS EXERCISES (continue from yesterday's file for this exercise)
+    1) Create two inputs for picking up colors in the page, and a button with "Apply Colors" as text
+    2) Grab all <li> in the page from the DOM, and save them in a variable called "listItems"
+    3) Grab the inputs references via DOM manipulation and save them in variables
+    4) Create a function and attach it to the "Apply Colors" button click event
+    5) Cycle listItems inside the function. Verify that the function and the loop work by logging in the console 
+        each item from the loop every time the button gets clicked
+    6) Move the inputs references in the function. Use the first input value as background-color for the item, and the second one for the text color.
+
+    If you receive black as a color check where you have the inputs references.
+    You should now see the list-items color and background-color change at the click of the button.
+--> */
